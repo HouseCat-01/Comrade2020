1fb38e62cbb840ac8f3acbd5b58d72af2cf43300
HouseCat-01 <taylor.housecat01@gmail.com>
2020-08-29 16:13:23 -0700

    Made stats static (global)

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -6,103 +6,99 @@ using UnityEngine.UI;
 
 public class ResourceManager : MonoBehaviour
 {
-    //tags
-    List<Tag> tags = new List<Tag>();
+    //UI interface
+    public static UIManager uiTracker;
 
-    //overall stats
-    public Text unityText;
-    public Text trustText;
-    public Text unrestText;
-    private int unity;
-    private int trust;
-    private int unrest;
+    //tags
+    public static List<Tag> tags = new List<Tag>();
+
+    //basic stats
+    private static int unity = 100;
+    private static int trust = 100;
+    private static int unrest = 0;
 
     //resources
-    public Text populationText;
-    public Text foodText;
-    public Text materialsText;
-    public Text machineryText;
-    private int population;
-    private int food;
-    private int materials;
-    private int machinery;
+    private static int population = 30000;
+    private static int food = 10;
+    private static int materials = 4;
+    private static int machinery = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiTracker = gameObject.GetComponent<UIManager>();
     }
 
-    public void AddUnity(int num) {
+    public static void AddUnity(int num) {
         SetUnity(num + unity);
     }
-    public void SetUnity(int num) {
+    public static void SetUnity(int num) {
         unity = num;
-        unityText.text = "Unity:" + unity;
+        uiTracker.UpdateText();
     }
-    public int GetUnity() {
+    public static int GetUnity() {
         return unity;
     }
-    public void AddTrust(int num) {
+    public static void AddTrust(int num) {
         SetTrust(num + trust);
     }
-    public void SetTrust(int num) {
+    public static void SetTrust(int num) {
         trust = num;
-        trustText.text = "Trust:" + trust;
+        uiTracker.UpdateText();
     }
-    public int GetTrust() {
+    public static int GetTrust() {
         return trust;
     }
-    public void AddUnrest(int num) {
+    public static void AddUnrest(int num) {
         SetUnrest(num + trust);
     }
-    public void SetUnrest(int num) {
+    public static void SetUnrest(int num) {
         unity = num;
-        unrestText.text = "Unrest:" + population;
+        uiTracker.UpdateText();
     }
-    public int GetUnrest() {
+    public static int GetUnrest() {
         return unrest;
     }
 
 
-    public void AddPopulation(int pop) {
+    public static void AddPopulation(int pop) {
         SetPopulation(pop + population);
     }
-    public void SetPopulation(int pop) {
+    public static void SetPopulation(int pop) {
         population = pop;
-        populationText.text = "Population:" + population;
+        uiTracker.UpdateText();
     }
-    public int GetPopulation() {
+    public static int GetPopulation() {
         return population;
     }
-    public void AddFood(int num) {
+    public static void AddFood(int num) {
         SetFood(num + food);
     }
-    public void SetFood(int num) {
+    public static void SetFood(int num) {
         food = num;
-        foodText.text = "Food:" + food;
+        uiTracker.UpdateText();
     }
-    public int GetFood() {
+    public static int GetFood() {
         return food;
     }
-    public void AddMaterials(int num) {
+    public static void AddMaterials(int num) {
         SetMaterials(num + materials);
     }
-    public void SetMaterials(int num) {
+    public static void SetMaterials(int num) {
         materials = num;
-        materialsText.text = "Materials:" + materials;
+        uiTracker.UpdateText();
     }
-    public int GetMaterials() {
+    public static int GetMaterials() {
         return materials;
     }
-    public void AddMachinery(int num) {
+    public static void AddMachinery(int num) {
         SetMachinery(num + machinery);
     }
-    public void SetMachinery(int num) {
+    public static void SetMachinery(int num) {
         machinery = num;
-        machineryText.text = "Machinery:" + machinery;
+        uiTracker.UpdateText();
     }
-    public int GetMachinery() {
+    public static int GetMachinery() {
         return machinery;
     }
 
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text unityText;
+    public Text trustText;
+    public Text unrestText;
+
+    public Text populationText;
+    public Text foodText;
+    public Text materialsText;
+    public Text machineryText;
+
+    public void UpdateText() {
+        unityText.text = "Unity:" + ResourceManager.GetUnity();
+        trustText.text = "Trust:" + ResourceManager.GetUnity();
+        unrestText.text = "Unrest:" + ResourceManager.GetUnity();
+
+        populationText.text = "Population:" + ResourceManager.GetUnity();
+        foodText.text = "Food:" + ResourceManager.GetUnity();
+        materialsText.text = "Materials:" + ResourceManager.GetUnity();
+        machineryText.text = "Machinery:" + ResourceManager.GetUnity();
+    }
+}
